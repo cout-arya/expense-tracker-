947a42cfa1381ad9b5569b8419707a43f9c1fd55
cout-arya <aryaverma888@gmail.com>
2026-01-16 01:50:38 +0530

    Fix wildcard route for Render deployment compatibility

--- server/index.js
+++ server/index.js
@@ -45,7 +45,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
 
   // Any route that is not an API route will be handled by the React app
-  app.get('*', (req, res) => {
+  app.get(/^(?!\/api).*$/, (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
   });
 } else {
